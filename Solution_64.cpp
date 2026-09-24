b3622bb8d98ee9c9ec1d3a137b9e1e3b11e38b0c
F**ckYOU <mygundonotwork@outlook.com>
2026-09-24 08:43:50 +0530

    DP is wild sometimes

--- Solution_64.cpp
+++ Solution_64.cpp
@@ -1,17 +1,22 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
-    int maxArea(vector<int>& h) {
-        int l = 0, r = h.size() - 1;
-        int mx = 0;
-        while (l < r) {
-            int area = min(h[l], h[r]) * (r - l);
-            mx = max(mx, area);
-            if (h[l] < h[r]) l++;
-            else r--;
+    int maxScore(vector<int>& cardPoints, int k) {
+        int n = cardPoints.size();
+        // initial sum of taking k cards from the left side
+        int currSum = 0;
+        for (int i = 0; i < k; ++i) currSum += cardPoints[i];
+        int maxSum = currSum;
+
+        // slide the window: each step remove one from left part and add one from right part
+        for (int i = k - 1, j = n - 1; i >= 0; --i, --j) {
+            currSum -= cardPoints[i];   // remove leftmost taken card
+            currSum += cardPoints[j];   // add rightmost card
+            maxSum = max(maxSum, currSum);
         }
-        return mx;
+        return maxSum;
     }
 };
