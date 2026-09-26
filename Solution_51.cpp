d3203dab7da486e42fbccf8c3a7cba520a35cb2f
F**ckYOU <mygundonotwork@outlook.com>
2026-09-26 18:36:43 +0530

    solved Solution 51

---
+++ Solution_51.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size(), st = 0, mx = 1;
+        auto expand = [&](int l, int r) {
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                if (r - l + 1 > mx) {
+                    mx = r - l + 1;
+                    st = l;
+                }
+                l--, r++;
+            }
+        };
+        for (int i = 0; i < n; i++) {
+            expand(i, i);
+            expand(i, i + 1);
+        }
+        return s.substr(st, mx);
+    }
+};
